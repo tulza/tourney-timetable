c7a7894cd0af5971396186c93697c5f4b6760d1b
Tulza <40633228+tulza@users.noreply.github.com>
2024-01-12 23:42:40 +1300

    deleted template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import Home from "./Template/Home";
-import Navigation from "./Template/Navigation";
+import Home from "./Components/Home";
 import "./styles.css";
 import {
   BrowserRouter as Router,
@@ -11,19 +10,14 @@ import {
 function App() {
   return (
     <Router basename="/tourney-timetable/">
-      <div className="App flex h-[100vh] flex-col justify-between">
-        <div className="Content m-20 flex h-[800px]  flex-col justify-center bg-black outline">
-          <Switch>
-            <Route path="/">
-              <Redirect to="/home" />
-            </Route>
-            <Route path="/home">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-        <Navigation />
-      </div>
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/">
+          <Redirect to="/home" />
+        </Route>
+      </Switch>
     </Router>
   );
 }
